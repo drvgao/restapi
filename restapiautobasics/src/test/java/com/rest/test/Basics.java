6ba5faec17460c960ead8aac4b3a42f0c0e10c46
Rajesh Durgam <rajesh.durgam@igt.com>
2018-04-03 14:06:27 +0530

    new tests added for headers.

--- restapiautobasics/src/test/java/com/rest/test/Basics.java
+++ restapiautobasics/src/test/java/com/rest/test/Basics.java
@@ -1,10 +1,12 @@
 package com.rest.test;
 
-import org.junit.Test;
 
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
+
+import org.junit.Test;
+
 
 public class Basics {
 	@Test
@@ -14,14 +16,34 @@ public class Basics {
 				.body("MRData.CircuitTable.Circuits.circuitId", hasSize(20));
 	}
 
-	@Test
+//	@Test
 	public void firstTest() {
 		// 1. base url
 		RestAssured.baseURI = "https://maps.googleapis.com";
 
 		// 2. parameters
-		given().param("query", "restaurants+in+Miyapur").param("key", "AIzaSyD05C4l0V-VS-4aM1mY5KQUdUE2xqu-GJo").when()
-				.get("/maps/api/place/textsearch/json").then().assertThat().statusCode(200)
-				.body("results.geometry.location.lat", contains(17.496931));
+		given().param("query", "restaurants+in+Miyapur").
+				param("key", "AIzaSyD05C4l0V-VS-4aM1mY5KQUdUE2xqu-GJo").
+		when()
+			.get("/maps/api/place/textsearch/json").
+		then().assertThat().statusCode(200).
+			body("results[0].geometry.location.lat", equalTo(17.496931));
+//			.body("results.geometry.location.lat", contains(17.496931));
 	}
+	
+	@Test
+	public void verifyNearByLocation() {
+		// 1. base url
+		RestAssured.baseURI = "https://maps.googleapis.com";
+
+		// 2. parameters
+		given().param("query", "restaurants+in+Miyapur").
+				param("key", "AIzaSyD05C4l0V-VS-4aM1mY5KQUdUE2xqu-GJo").
+		when()
+			.get("/maps/api/place/textsearch/json").
+		then().assertThat().statusCode(200).
+			body("results[0].name", equalTo("Angaara Restaurant"));
+	}
+	
+	
 }
