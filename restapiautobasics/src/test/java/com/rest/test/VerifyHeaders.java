e421a68a562304f9e734c2585801b869df6ef9c8
Rajesh Durgam <rajesh.durgam@igt.com>
2018-04-04 09:11:04 +0530

    header test added

---
+++ restapiautobasics/src/test/java/com/rest/test/VerifyHeaders.java
@@ -0,0 +1,26 @@
+package com.rest.test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+
+public class VerifyHeaders {
+	
+	@Test
+	public void verifyHeader() {
+		// 1. base url
+		RestAssured.baseURI = "https://maps.googleapis.com";
+
+		// 2. parameters
+		given().param("query", "restaurants+in+Miyapur").
+				param("key", "AIzaSyD05C4l0V-VS-4aM1mY5KQUdUE2xqu-GJo").
+		when()
+			.get("/maps/api/place/textsearch/json").
+		then().assertThat().statusCode(200).
+			body("results[0].name", equalTo("Angaara Restaurant")).and().
+			header("Server", "scaffolding on HTTPServer2");
+	}
+}
